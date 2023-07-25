ec6d53df2b896487b3c605f4b05e2fc36314d035
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-25 13:17:30 -0700

    styling login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-
-export const Login = () =>{
+import '../css/login.css'
+export const Login = () => {
     const CLIENT_ID = "bd082bb71cdf43fdbe70dd6eb449d50d"
     const REDIRECT_URI = "https://react-spotify-mocha.vercel.app/home"
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
     const RESPONSE_TYPE = "token"
 
-    return(
-        <div>
-            <h1> Login Page </h1>
-            <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-            > <button className = "btn btn-success"> Login with Spotify </button> </a>
+    return (
+        <div id="login-con">
+            <div className="d-flex justify-content-center h-100">
+                <div className="card" id="login-card">
+                    <div className="card-header">
+                        <h3>Welcome to React Spotify </h3>
+                    </div>
+
+                    <div className="card-body">
+                        <h3> With React Spotify, you can view your top songs and artists</h3>
+                        <h3> To get started, sign in with your Spotify account</h3>
+                    </div>
+
+                    <div className="card-footer">
+                        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+                        > <button className="btn btn-success"> Login with Spotify </button> </a>
+                    </div>
+                </div>
+            </div>
+
+            
         </div>
     )
-    
+
 }
