521600bc98b4604bf11ea8ba0d677d3d09366c8c
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-26 17:20:02 -0700

    styled login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,20 +10,26 @@ export const Login = () => {
     const RESPONSE_TYPE = "token"
 
     return (
-        <div>
-            <header className="login-header"> Spotify Summary </header>
-
-            <div className="login-card">
-                <div className="login-text"> Log in to Spotify </div>
-
-                <div> <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-                > <button className="login-button btn btn-success"> Log In  </button> </a>
+        <div className="container">
+            <div className="body d-md-flex align-items-center justify-content-between">
+                <div className="box-1 mt-md-0 mt-5">
+                    <img src="https://images.pexels.com/photos/2033997/pexels-photo-2033997.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
                 </div>
-                
+                <div className=" box-2 d-flex flex-column h-100">
+                    <div className="mt-5">
+                        <p className="mb-1 h-1"> Spotify Summary</p>
+                        <div className="d-flex flex-column ">
+                            <div className="align-items-center">
+                                <button className="btn btn-success"> Log in to Spotify </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p className="after-text"> See and share your top songs and artists </p>
+                </div>
+                <span className="fas fa-times" />
             </div>
         </div>
-
-
     )
 
 }
