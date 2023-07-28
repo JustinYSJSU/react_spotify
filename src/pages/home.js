6db8a61f1c8f4d6f6ea28b5fb9323f2f2110a956
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-28 14:24:23 -0700

    sztdxcfgvh

--- src/pages/home.js
+++ src/pages/home.js
@@ -38,11 +38,34 @@ export const Home = () => {
         <div className="container">
             <div className="body-1 d-md-flex align-items-center justify-content-between">
                 <div className="card-header">
-                   Welcome to Spotify Summary, {displayName}!
+                    Welcome to Spotify Summary, {displayName}!
                 </div>
 
                 <div className="card-body">
                     Use the menu below to choose your category, number of items, and time range
+                </div>
+
+                <div className="card-select">
+                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected> Category </option>
+                        <option value="songs"> Songs </option>
+                        <option value="artists"> Artists </option>
+                    </select>
+
+                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected> Return Number </option>
+                        <option value="10"> 10 </option>
+                        <option value="15"> 15 </option>
+                        <option value="20"> 20 </option>
+                    </select>
+
+                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected> Time Period </option>
+                        <option value="short_term"> Past Month </option>
+                        <option value="medium_term"> Past 6 Months  </option>
+                        <option value="long_term" > All Time </option>
+                    </select>
+
                 </div>
             </div>
         </div>
