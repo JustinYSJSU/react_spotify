bf0bea58395d14cc18a9f8ed609fba651f3227fd
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-29 12:29:19 -0700

    remove method

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,8 +10,7 @@ export const Home = () => {
     const getDisplayName = async () => {
         const { data } = await axios.get("https://api.spotify.com/v1/me", {
             headers: {
-                method: 'GET',
-                Authorization: `Bearer ${token}`,
+                 Authorization: `Bearer ${token}`,
 
             }
         })
