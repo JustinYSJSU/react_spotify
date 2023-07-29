c3a11a28c1486ee4821caf5430027fa9fd58888e
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-29 13:05:29 -0700

    align select menu

--- src/pages/home.js
+++ src/pages/home.js
@@ -54,26 +54,32 @@ export const Home = () => {
                 </div>
 
                 <div className="card-select">
-                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" required>
+                    <div className="select-wrapper">
+                     <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" required>
                         <option selected> Category... </option>
                         <option value="songs"> Songs </option>
                         <option value="artists"> Artists </option>
-                    </select>
+                     </select>
+                    </div>
 
+                   <div className="select-wrapper">
                     <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" required>
                         <option selected> Return Number... </option>
                         <option value="10"> 10 </option>
                         <option value="15"> 15 </option>
                         <option value="20"> 20 </option>
                     </select>
+                   </div>
 
+                   <div className="select-wrapper">
                     <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" required>
                         <option selected> Time Period... </option>
                         <option value="short_term"> Past Month </option>
                         <option value="medium_term"> Past 6 Months  </option>
                         <option value="long_term" > All Time </option>
                     </select>
-
+                   </div>
+                   
                 </div>
             </div>
         </div>
