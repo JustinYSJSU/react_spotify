c651156be29357b1467fd184f8ee01c3a0cc198b
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-30 13:12:26 -0700

    remove type os

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,6 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap
 import { useNavigate } from "react-router-dom";
-import { type } from "os";
 
 export const Home = () => {
 
