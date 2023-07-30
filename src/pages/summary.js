e9383302b091f5339ce321b34a183b9071cc0dcd
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-30 13:16:51 -0700

    text visibility

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
-export const TopArtistsSongs = () =>{
+export const Summary = () =>{
     const {displayName} = useParams()
     const {top} = useParams()
     const {type} = useParams()
     const {past} = useParams()
 
     return(
-        <p>
+        <h1 className="initial">
             {displayName} {top} {type} {past}
-        </p>
+         </h1>
     )
 }
