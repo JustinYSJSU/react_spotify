829fdaca3940c6d33da8afc3ba9245fbb635096e
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 01:51:45 -0700

    xtfcghvjbjklk;

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import "../css/summary.css"
 
 export const Summary = () =>{
     const {displayName} = useParams()
