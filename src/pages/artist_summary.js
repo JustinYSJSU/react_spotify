b5404b24aefe2299ccdef96bf79ab4e6257c67bd
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 12:57:04 -0700

    add scope

--- src/pages/artist_summary.js
+++ src/pages/artist_summary.js
@@ -8,6 +8,27 @@ export const ArtistSummary = () =>{
     const {type} = useParams()
     const {past} = useParams()
 
+    useEffect(() => {
+        const hash = window.location.hash //from URL
+        console.log(hash)
+        let token = window.localStorage.getItem("token")
+        console.log(token)
+
+        if (!token && hash) { //no token, but hash. get the token from the hash using .split() and .find() 
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"))?.split("=")[1]
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+
+        }
+
+        setToken(token)
+        console.log(token)
+        getDisplayName()
+    }, [])
+
+    useEffect(() => {
+        token && getDisplayName()
+    }, [token])
     return(
         <h1 className="initial">
             {displayName} {top} {type} {past}
--- src/pages/track_summary.js
+++ src/pages/track_summary.js
@@ -8,6 +8,21 @@ export const TrackSummary = () =>{
     const {type} = useParams()
     const {past} = useParams()
 
+    const getTrackData = async () => {
+        console.log("Token: ", token)
+        const { data } = await axios.get("https://api.spotify.com/v1/me", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+
+            }
+        })
+        setDisplayName(data.display_name)
+        setUri(data.uri)
+        console.log(data)
+        console.log(data.display_name)
+        console.log(data.uri)
+    }
+    
     return(
         <h1 className="initial">
             {displayName} {top} {type} {past}
