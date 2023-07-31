b43619913c3ef98d01ec11d6f7f4bb94ee5da99c
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 16:22:16 -0700

    try artist

--- src/pages/artist_summary.js
+++ src/pages/artist_summary.js
@@ -3,16 +3,16 @@ import { useParams } from "react-router-dom"
 import "../css/summary.css"
 import axios from "axios"
 
-export const ArtistSummary = () =>{
+export const ArtistSummary = () => {
     const [token, setToken] = useState("")
     const [artistList, setArtistList] = useState([])
-    const {displayName} = useParams()
-    const {top} = useParams()
-    const {past} = useParams()
+    const { displayName } = useParams()
+    const { top } = useParams()
+    const { past } = useParams()
 
-    const url = window.location.href 
+    const url = window.location.href
 
-    const copyLink = async () =>{
+    const copyLink = async () => {
         await navigator.clipboard.writeText(url)
         alert("Link copied and ready to share!")
     }
@@ -47,7 +47,39 @@ export const ArtistSummary = () =>{
         token && getArtistSummary()
     }, [token])
 
-    return(
-        <h1> hi </h1>
+    return (
+        <div className="table-container">
+            <div className="container-summary mt-4">
+                <h2> {displayName}, here is your Spotify Summary! </h2>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Number</th>
+                            <th>Artist</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {artistList.map((artist) => (
+                            <tr key={artist.name} className="song-row">
+                                <td>{artistList.indexOf(artist) + 1}</td>
+                                <td>
+                                    <div className="media">
+                                        <img src={artist.images[2].url} className="mr-3" alt={artist.name} style={{ maxWidth: 100 }} />
+                                        <div className="media-body">
+                                            <h5 className="mt-0 song-title">{artist.name}</h5>
+                                                <p className="faded-text">{artist.genres[0]}</p>
+                                            <div className="listen-now">
+                                                <a href={artist.external_urls.spotify}> Listen Now </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <button onClick={copyLink} className="btn btn-primary"> Copy Link </button>
+            </div>
+        </div>
     )
 }
