480939e03b612ceaaf203f8299dabb64956e805b
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 16:33:27 -0700

    table responsiveness?

--- src/pages/track_summary.js
+++ src/pages/track_summary.js
@@ -10,9 +10,9 @@ export const TrackSummary = () => {
     const { top } = useParams()
     const { past } = useParams()
 
-    const url = window.location.href 
+    const url = window.location.href
 
-    const copyLink = async () =>{
+    const copyLink = async () => {
         await navigator.clipboard.writeText(url)
         alert("Link copied and ready to share!")
     }
@@ -52,36 +52,38 @@ export const TrackSummary = () => {
         <div className="table-container">
             <div className="container-summary mt-4">
                 <h2> {displayName}, here is your Spotify Summary! </h2>
-                <table className="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Number</th>
-                            <th>Song</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {trackList.map((track) => (
-                            <tr key={track.name} className="song-row">
-                                <td>{trackList.indexOf(track) + 1}</td>
-                                <td>
-                                    <div className="media">
-                                        <img src={track.album.images[2].url} className="mr-3" alt={track.title} style={{ maxWidth: 100 }} />
-                                        <div className="media-body">
-                                            <h5 className="mt-0 song-title">{track.name}</h5>
-                                            {track.album.artists.map((artist) => (
-                                                <p className="faded-text">{artist.name}</p>
-                                            ))}
-                                            <div className="listen-now">
-                                                <a href={track.external_urls.spotify}>Listen Now</a>
+                <div className="table-responsive">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Number</th>
+                                <th>Song</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {trackList.map((track) => (
+                                <tr key={track.name} className="song-row">
+                                    <td>{trackList.indexOf(track) + 1}</td>
+                                    <td>
+                                        <div className="media">
+                                            <img src={track.album.images[2].url} className="mr-3" alt={track.title} style={{ maxWidth: 100 }} />
+                                            <div className="media-body">
+                                                <h5 className="mt-0 song-title">{track.name}</h5>
+                                                {track.album.artists.map((artist) => (
+                                                    <p className="faded-text">{artist.name}</p>
+                                                ))}
+                                                <div className="listen-now">
+                                                    <a href={track.external_urls.spotify}>Listen Now</a>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <button onClick={copyLink} className="btn btn-primary"> Copy Link </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <button onClick={copyLink} className="btn btn-primary"> Copy Link </button>
+                </div>
             </div>
         </div>
 
