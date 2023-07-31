dc06b4dbeaa81fde4eeef0ac443e9ab85abc9007
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 14:37:23 -0700

    table tseatse

--- src/pages/track_summary.js
+++ src/pages/track_summary.js
@@ -3,16 +3,16 @@ import { useParams } from "react-router-dom"
 import "../css/summary.css"
 import axios from "axios"
 
-export const TrackSummary = () =>{
+export const TrackSummary = () => {
     const [token, setToken] = useState("")
     const [trackList, setTrackList] = useState([])
-    const {displayName} = useParams()
-    const {top} = useParams()
-    const {type} = useParams()
-    const {past} = useParams()
+    const { displayName } = useParams()
+    const { top } = useParams()
+    const { type } = useParams()
+    const { past } = useParams()
 
-    const getTrackSummary = async () =>{
-      const { data } = await axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${past}&limit=${top}`,{
+    const getTrackSummary = async () => {
+        const { data } = await axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${past}&limit=${top}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             }
@@ -44,7 +44,7 @@ export const TrackSummary = () =>{
 
 
 
-    return(
+    return (
         /*
         <div className="track-container">
             <div className="body-track">
@@ -70,36 +70,91 @@ export const TrackSummary = () =>{
             </div>
         </div>
         */
-        <table class="table">
+        <table className="table align-middle mb-0 bg-white">
+            <thead className="bg-light">
+                <tr>
+                    <th>Name</th>
+                    <th>Title</th>
+                    <th>Status</th>
+                    <th>Position</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div className="d-flex align-items-center">
+                            <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt style={{ width: 45, height: 45 }} className="rounded-circle" />
+                            <div className="ms-3">
+                                <p className="fw-bold mb-1">John Doe</p>
+                                <p className="text-muted mb-0">john.doe@gmail.com</p>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <p className="fw-normal mb-1">Software engineer</p>
+                        <p className="text-muted mb-0">IT department</p>
+                    </td>
+                    <td>
+                        <span className="badge badge-success rounded-pill d-inline">Active</span>
+                    </td>
+                    <td>Senior</td>
+                    <td>
+                        <button type="button" className="btn btn-link btn-sm btn-rounded">
+                            Edit
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex align-items-center">
+                            <img src="https://mdbootstrap.com/img/new/avatars/6.jpg" className="rounded-circle" alt style={{ width: 45, height: 45 }} />
+                            <div className="ms-3">
+                                <p className="fw-bold mb-1">Alex Ray</p>
+                                <p className="text-muted mb-0">alex.ray@gmail.com</p>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <p className="fw-normal mb-1">Consultant</p>
+                        <p className="text-muted mb-0">Finance</p>
+                    </td>
+                    <td>
+                        <span className="badge badge-primary rounded-pill d-inline">Onboarding</span>
+                    </td>
+                    <td>Junior</td>
+                    <td>
+                        <button type="button" className="btn btn-link btn-rounded btn-sm fw-bold" data-mdb-ripple-color="dark">
+                            Edit
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex align-items-center">
+                            <img src="https://mdbootstrap.com/img/new/avatars/7.jpg" className="rounded-circle" alt style={{ width: 45, height: 45 }} />
+                            <div className="ms-3">
+                                <p className="fw-bold mb-1">Kate Hunington</p>
+                                <p className="text-muted mb-0">kate.hunington@gmail.com</p>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <p className="fw-normal mb-1">Designer</p>
+                        <p className="text-muted mb-0">UI/UX</p>
+                    </td>
+                    <td>
+                        <span className="badge badge-warning rounded-pill d-inline">Awaiting</span>
+                    </td>
+                    <td>Senior</td>
+                    <td>
+                        <button type="button" className="btn btn-link btn-rounded btn-sm fw-bold" data-mdb-ripple-color="dark">
+                            Edit
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
 
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col"> Name </th>
-            <th scope="col"> Artist </th>
-            <th scope="col"> Spotify Link </th>
-          </tr>
-        </thead>
-        
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
     )
 }
