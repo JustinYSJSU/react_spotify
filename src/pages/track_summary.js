0c83c0255a28f368f65447dd232b440d07459305
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 14:56:51 -0700

    table??

--- src/pages/track_summary.js
+++ src/pages/track_summary.js
@@ -45,116 +45,41 @@ export const TrackSummary = () => {
 
 
     return (
-        /*
-        <div className="track-container">
-            <div className="body-track">
-            {trackList.map( (track) => {
-                return(
-                    <div className="summary-entry"> 
-                      <p className="name">
-                        {track.name}
-                      </p>
+        <div className="container mt-4">
+            <h2> Song List</h2>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Number</th>
+                        <th>Song</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {trackList.map((track) => (
+                        <tr key={track.name} className="song-row">
+                            <td>{track.id}</td>
+                            <td>
+                                <div className="media">
+                                    <img src={track.album.images[2].url} className="mr-3" alt={track.title} style={{ maxWidth: 100 }} />
+                                    <div className="media-body">
+                                        <h5 className="mt-0">{track.name}</h5>
+                                        {track.album.artists.map( (artist) => {
+                                            <p className="faded-text">{artist} </p>
+                                        })}
 
-                      <img src={track.album.images[2].url}/>
-
-                      {track.artists.map( (artist) => {
-                        return(<p className="text-muted artist">
-                            {artist.name}
-                        </p>
-                        )
-                      })}
-                      <a href={track.external_urls.spotify}> <button className="btn btn-success track"> Listen on Spotify </button></a>                  
-                    </div>
-                )
-            })}
-            </div>
+                                        <div className="listen-now">
+                                            <a href={track.external_urls.spotify}>Listen Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-        */
-        <table className="table align-middle mb-0 bg-white">
-            <thead className="bg-light">
-                <tr>
-                    <th>Name</th>
-                    <th>Title</th>
-                    <th>Status</th>
-                    <th>Position</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <div className="d-flex align-items-center">
-                            <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt style={{ width: 45, height: 45 }} className="rounded-circle" />
-                            <div className="ms-3">
-                                <p className="fw-bold mb-1">John Doe</p>
-                                <p className="text-muted mb-0">john.doe@gmail.com</p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <p className="fw-normal mb-1">Software engineer</p>
-                        <p className="text-muted mb-0">IT department</p>
-                    </td>
-                    <td>
-                        <span className="badge badge-success rounded-pill d-inline">Active</span>
-                    </td>
-                    <td>Senior</td>
-                    <td>
-                        <button type="button" className="btn btn-link btn-sm btn-rounded">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className="d-flex align-items-center">
-                            <img src="https://mdbootstrap.com/img/new/avatars/6.jpg" className="rounded-circle" alt style={{ width: 45, height: 45 }} />
-                            <div className="ms-3">
-                                <p className="fw-bold mb-1">Alex Ray</p>
-                                <p className="text-muted mb-0">alex.ray@gmail.com</p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <p className="fw-normal mb-1">Consultant</p>
-                        <p className="text-muted mb-0">Finance</p>
-                    </td>
-                    <td>
-                        <span className="badge badge-primary rounded-pill d-inline">Onboarding</span>
-                    </td>
-                    <td>Junior</td>
-                    <td>
-                        <button type="button" className="btn btn-link btn-rounded btn-sm fw-bold" data-mdb-ripple-color="dark">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div className="d-flex align-items-center">
-                            <img src="https://mdbootstrap.com/img/new/avatars/7.jpg" className="rounded-circle" alt style={{ width: 45, height: 45 }} />
-                            <div className="ms-3">
-                                <p className="fw-bold mb-1">Kate Hunington</p>
-                                <p className="text-muted mb-0">kate.hunington@gmail.com</p>
-                            </div>
-                        </div>
-                    </td>
-                    <td>
-                        <p className="fw-normal mb-1">Designer</p>
-                        <p className="text-muted mb-0">UI/UX</p>
-                    </td>
-                    <td>
-                        <span className="badge badge-warning rounded-pill d-inline">Awaiting</span>
-                    </td>
-                    <td>Senior</td>
-                    <td>
-                        <button type="button" className="btn btn-link btn-rounded btn-sm fw-bold" data-mdb-ripple-color="dark">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+
 
     )
+
 }
