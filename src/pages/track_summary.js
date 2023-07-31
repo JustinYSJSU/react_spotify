c957cc4873d5ce08c0d43ed7991a37410ec1891e
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-07-31 14:29:36 -0700

    test table

--- src/pages/track_summary.js
+++ src/pages/track_summary.js
@@ -45,6 +45,7 @@ export const TrackSummary = () =>{
 
 
     return(
+        /*
         <div className="track-container">
             <div className="body-track">
             {trackList.map( (track) => {
@@ -68,5 +69,37 @@ export const TrackSummary = () =>{
             })}
             </div>
         </div>
+        */
+        <table class="table">
+
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col"> Name </th>
+            <th scope="col"> Artist </th>
+            <th scope="col"> Spotify Link </th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     )
 }
