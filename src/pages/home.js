d546cafdc1e5f2756d4e41ed74a3c60dbf36ec40
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-08-02 14:26:24 -0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,8 +29,8 @@ export const Home = () => {
         fetch('https://accounts.spotify.com/api/token', authParameters)
         .then(result => result.json())
         .then(data => setAccessToken(data.access_token))
-        console.log(accessToken)
     }, [])
+    console.log(accessToken)
 
     /*
     const getDisplayName = async () => {
